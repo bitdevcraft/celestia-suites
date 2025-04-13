f86e3b57d67bd30c276b7efdc72f0def38846758
The Bit Dev <ryanvincent.capio@outlook.com>
2025-04-13 15:50:51 +0400

    fix the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <nav className="max-w-7xl mx-auto py-4 px-4 absolute top-0 right-0 left-0 z-999">
+    <nav className="max-w-7xl mx-auto py-6 px-4 absolute top-0 right-0 left-0 z-999">
       <div className="flex justify-between items-center fixed-top-0">
-        <div className="flex flex-col justify-center items-center text-josefin-sans text-brand w-full">
+        <div className="flex flex-col justify-center items-center font-josefin-sans text-brand w-full">
           <p>
             <span className="text-[3em]/10">C</span>
             <span className="text-[2.4em]/10">ELESTIA</span>
